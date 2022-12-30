e81e692b057536b31a9712d18aa7352afe7c745f
Azz <asteria@valera.co>
2022-12-30 15:08:56 +0000

    all okay

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -6,8 +6,10 @@ export const load = (async ({ platform }) => {
 
   const start = Date.now();
   if (platform) {
+    console.log("using zero-lat")
     post = await platform.env.API.fetch("https://api.valera.co/test")
   } else {
+    console.log("zero-lat not available, using https")
     post = await fetch("https://api.valera.co/test")
   }
   const lat = Date.now() - start;
